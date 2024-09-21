d5adf721e264048f07b398a22d165d7c94bf001f
Naingui <aaron.groux@epitech.eu>
2024-09-21 15:57:38 -0600

    fix Connect to server

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './src/routes/userRoutes';
+import { setupSwagger } from './src/swagger';
 
 dotenv.config();
 
@@ -8,6 +9,7 @@ const app = express();
 
 app.use(express.json());
 app.use('/api/users', userRoutes);
+setupSwagger(app);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
